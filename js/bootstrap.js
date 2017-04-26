e38eb031c2c7ffefc3f38faf9ba9b0d1ddda141f
aroharia <aroharia@gmail.com>
2017-04-26 14:16:43 -0500

    major edits to theme + popup window testing

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -8,6 +8,13 @@ if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
 }
 
+// When the user clicks on <div>, open the popup
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+
+
 +function ($) {
   'use strict';
   var version = $.fn.jquery.split(' ')[0].split('.')
